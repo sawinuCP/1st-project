80cae54f3e77ff2c473a50b5f7ddd3d0751a5c69
Sawinu Chandira Peiris <savinu.chandira2002@gmail.com>
2023-05-10 19:38:56 +0530

    cwec

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import NewsCard from "./components/NewsCard";
 import "./styles/index.scss";
+import { getNews } from "./utils/api";
 
 function App() {
 
+  const[changer , setChanger] = useState(true);
+
   useEffect(()=>{
-    console.info("inside the useeffect")
+    async function getData(){
+      await getNews();
+    }
+
+    getData();
+    // console.info("inside the useeffect")
   }, []);
   
   return (
     <>
     < Navbar/>
+
+    <button onClick={()=>{setChanger(false)}}></button>
     <div>
       < NewsCard newsTitle={"test"} newsDescription={"test description"}/>
     </div>
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,3 +1,19 @@
 import axios from "axios"
 
 const BASE_API = axios.create({baseURL:"http://localhost:5050/api/v1/" })
+
+
+async function getNews(){
+    try {
+        const res = await BASE_API.get("/news/get");
+        console.log(res.data)
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export {getNews}
+
+
+
